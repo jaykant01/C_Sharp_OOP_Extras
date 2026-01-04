b9cd91cb5b6a58020eedd5a96075a1e8a4e10efd
jaykant01 <jaykanty24092004@gmail.com>
2026-01-05 03:44:28 +0530

    [Jaykant] Add .UC 4 making Abstract to vehicle class

--- OOP_Extra_Problems/ElectricCar.cs
+++ OOP_Extra_Problems/ElectricCar.cs
@@ -2,5 +2,46 @@
 
 public class ElectricCar
 {
+    public int SeatingCapacity { get; private set; }
 
+    // Constructor calls parent constructor
+    public ElectricCar(string vehicleId, string model, int batteryPercentage, int seatingCapacity)
+        : base(vehicleId, model, batteryPercentage)
+    {
+        SeatingCapacity = seatingCapacity;
+    }
+
+    public void DisplayCarDetails()
+    {
+        DisplayDetails();
+        Console.WriteLine($"Seating Capacity: {SeatingCapacity}");
+    }
+
+    public override double CalculateTripCost(double distanceInKm)
+    {
+        return distanceInKm * 15;
+    }
+}
+
+public class ElectricScooter : Vehicle
+{
+    public int MaxSpeedLimit { get; private set; }
+
+    // Constructor calls parent constructor
+    public ElectricScooter(string vehicleId, string model, int batteryPercentage, int maxSpeedLimit)
+        : base(vehicleId, model, batteryPercentage)
+    {
+        MaxSpeedLimit = maxSpeedLimit;
+    }
+
+    public void DisplayScooterDetails()
+    {
+        DisplayDetails();
+        Console.WriteLine($"Max Speed Limit: {MaxSpeedLimit} km/h");
+    }
+
+    public override double CalculateTripCost(double distanceInKm)
+    {
+        return distanceInKm * 7; 
+    }
 }
